bb6db1588d561f88c5e32c912ea65337e7d4d2dc
Olga Tsybikova <tsy2701@gmail.com>
2022-05-15 22:17:07 +0200

    Changed try catch in thunks to avoide json promise error again

--- src/redux/thunks/robotsThunk.js
+++ src/redux/thunks/robotsThunk.js
@@ -2,10 +2,12 @@ import axios from "axios";
 import { loadRobotsActionCreator } from "../feature/robotSlice";
 const loadRobotsActionThunk = () => async (dispatch) => {
   try {
-    const { data: robots } = await axios.get(process.env.REACT_APP_ROBOTS);
-    dispatch(loadRobotsActionCreator(robots.robots));
-  } catch (error) {
-    return error;
-  }
+    const { data: robots, status } = await axios.get(
+      process.env.REACT_APP_ROBOTS
+    );
+    if (status === 200) {
+      dispatch(loadRobotsActionCreator(robots.robots));
+    }
+  } catch {}
 };
 export default loadRobotsActionThunk;
