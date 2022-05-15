628b8f81f30a06eb103a249f191d09acd9aa2cf9
Olga Tsybikova <tsy2701@gmail.com>
2022-05-15 14:09:38 +0200

    Added component robotlist

---
+++ src/components/Robot/Robot.js
@@ -0,0 +1,12 @@
+const Robot = () => {
+  return (
+    <>
+      <li>
+        <h2>Robot's Name</h2>
+        <img src="url" alt="robot" />
+        <span>info</span>
+      </li>
+    </>
+  );
+};
+export default Robot;
