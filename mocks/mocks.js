4ccfbe5e926ce28e5d316588298cd011e02c5130
Olga Tsybikova <tsy2701@gmail.com>
2022-05-16 23:37:35 +0200

    Tested part of robot list component

---
+++ mocks/mocks.js
@@ -0,0 +1,49 @@
+const mockRobots = [
+  {
+    id: "robot",
+    name: "Eva",
+    image:
+      "https://i.pinimg.com/originals/b7/b3/9e/b7b39e8dbd8649056c076c2b8b73b1a6.jpg",
+    created: 2008,
+    resilience: 7,
+    speed: 10,
+  },
+  {
+    id: "6280cb74612f27bd23c70c89",
+    name: "Buddy",
+    image:
+      "https://cdn.pocket-lint.com/r/s/660x/assets/images/149940-feature-cute-robots-image2-kjz1elwvnr-jpg.webp?v1",
+    created: 2017,
+    resilience: 6,
+    speed: 4,
+  },
+  {
+    id: "6280cc14612f27bd23c70c8a",
+    name: "Paro",
+    image:
+      "https://www.addinformatica.com/wp-content/uploads/2015/08/paro-foca-1024x550.jpg",
+    created: 2003,
+    resilience: 8,
+    speed: 1,
+  },
+  {
+    id: "6280cce6612f27bd23c70c8b",
+    name: "Somnox sleep robot",
+    image:
+      "https://i.guim.co.uk/img/media/d126b38c7ea164b8f64031bcc69591098b59f259/159_645_6392_3835/master/6392.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=bbbc2f205a044a7fb8bbbb12290fb39b",
+    created: 2016,
+    resilience: 5,
+    speed: 0,
+  },
+  {
+    id: "6280cd74612f27bd23c70c8c",
+    name: "Woobo",
+    image:
+      "https://www.ubuy.com.gr/productimg/?image=aHR0cHM6Ly9pbWFnZXMtbmEuc3NsLWltYWdlcy1hbWF6b24uY29tL2ltYWdlcy9JLzYxd0NVazlBQTlMLl9TUzQwMF8uanBn.jpg",
+    created: 2020,
+    resilience: 8,
+    speed: 1,
+  },
+];
+
+module.exports = { mockRobots };
