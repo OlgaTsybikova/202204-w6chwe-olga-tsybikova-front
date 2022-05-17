982062a6d1ec53a675ff0189b824170d219e4d50
Olga Tsybikova <tsy2701@gmail.com>
2022-05-17 17:56:11 +0200

    Tested load thunk

--- mocks/mocks.js
+++ mocks/mocks.js
@@ -1,48 +1,122 @@
 const mockRobots = [
   {
-    id: "robot",
-    name: "Eva",
-    image:
-      "https://i.pinimg.com/originals/b7/b3/9e/b7b39e8dbd8649056c076c2b8b73b1a6.jpg",
-    created: 2008,
-    resilience: 7,
-    speed: 10,
-  },
-  {
-    id: "6280cb74612f27bd23c70c89",
+    _id: "6280cb74612f27bd23c70c89",
     name: "Buddy",
     image:
       "https://cdn.pocket-lint.com/r/s/660x/assets/images/149940-feature-cute-robots-image2-kjz1elwvnr-jpg.webp?v1",
     created: 2017,
     resilience: 6,
     speed: 4,
+    user: "62827228612f27bd23c70ccf",
   },
   {
-    id: "6280cc14612f27bd23c70c8a",
+    _id: "6280cc14612f27bd23c70c8a",
     name: "Paro",
     image:
       "https://www.addinformatica.com/wp-content/uploads/2015/08/paro-foca-1024x550.jpg",
     created: 2003,
     resilience: 8,
     speed: 1,
+    user: "62827228612f27bd23c70ccf",
   },
   {
-    id: "6280cce6612f27bd23c70c8b",
+    _id: "6280cce6612f27bd23c70c8b",
     name: "Somnox sleep robot",
     image:
       "https://i.guim.co.uk/img/media/d126b38c7ea164b8f64031bcc69591098b59f259/159_645_6392_3835/master/6392.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=bbbc2f205a044a7fb8bbbb12290fb39b",
     created: 2016,
     resilience: 5,
     speed: 0,
+    user: "62827228612f27bd23c70ccf",
   },
   {
-    id: "6280cd74612f27bd23c70c8c",
+    _id: "6280cd74612f27bd23c70c8c",
     name: "Woobo",
     image:
       "https://www.ubuy.com.gr/productimg/?image=aHR0cHM6Ly9pbWFnZXMtbmEuc3NsLWltYWdlcy1hbWF6b24uY29tL2ltYWdlcy9JLzYxd0NVazlBQTlMLl9TUzQwMF8uanBn.jpg",
     created: 2020,
     resilience: 8,
     speed: 1,
+    user: "62827228612f27bd23c70ccf",
+  },
+  {
+    _id: "6280ce4f612f27bd23c70c8d",
+    name: "Kiki",
+    image:
+      "https://grupoadd.es/wp-content/uploads/2019/09/Robot-Kiki-3-Grupo-ADD.jpg",
+    created: 2019,
+    resilience: 9,
+    speed: 4,
+    user: "62827228612f27bd23c70ccf",
+  },
+  {
+    _id: "6280ceee612f27bd23c70c8e",
+    name: "Bocco Emo",
+    image:
+      "https://uploads.ifdesign.de/award_img_346/oex_large/310085_01_a.jpg",
+    created: 2019,
+    resilience: 7,
+    speed: 0,
+    user: "62827228612f27bd23c70ccf",
+  },
+  {
+    _id: "6280cf67612f27bd23c70c8f",
+    name: "Lovot - the love robot",
+    image:
+      "https://i.pinimg.com/736x/b6/d8/35/b6d83500bf520aace900022798e43438.jpg",
+    created: 2018,
+    resilience: 7,
+    speed: 6,
+    user: "62827228612f27bd23c70ccf",
+  },
+  {
+    _id: "6280cfcc612f27bd23c70c90",
+    name: "Murata",
+    image:
+      "https://corporate.murata.com/-/media/corporate/more_murata/robots/top/2014_01.ashx",
+    created: 2010,
+    resilience: 7,
+    speed: 9,
+    user: "62827228612f27bd23c70ccf",
+  },
+  {
+    _id: "6280d062612f27bd23c70c91",
+    name: "Robohon",
+    image:
+      "https://geekytheory.com/content/images/2015/10/robohon_-1024x640.png",
+    created: 2015,
+    resilience: 7,
+    speed: 5,
+    user: "62827228612f27bd23c70ccf",
+  },
+  {
+    _id: "6280d111612f27bd23c70c92",
+    name: "Robear",
+    image:
+      "https://vid.alarabiya.net/images/2015/02/24/1ba2cbbe-220a-4e11-8e5b-154ece87342d/1ba2cbbe-220a-4e11-8e5b-154ece87342d_16x9_1200x676.jpg",
+    created: 2015,
+    resilience: 10,
+    speed: 4,
+    user: "62827228612f27bd23c70ccf",
+  },
+  {
+    _id: "6280d175612f27bd23c70c93",
+    name: "Robi Robot",
+    image:
+      "https://boombastic.es/wp-content/uploads/2017/03/robi-robot2-1-960x960.jpg",
+    created: 2016,
+    resilience: 6,
+    speed: 4,
+    user: "62827228612f27bd23c70ccf",
+  },
+  {
+    _id: "6280d1dd612f27bd23c70c94",
+    name: "Yume Neko Smile",
+    image: "https://m.media-amazon.com/images/I/41OL1i-KESL.jpg",
+    created: 2007,
+    resilience: 5,
+    speed: 3,
+    user: "62827228612f27bd23c70ccf",
   },
 ];
 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import RobotList from "./components/RobotList/RobotList";
-import loadRobotsActionThunk from "./redux/thunks/robotsThunk";
+import loadRobotsThunk from "./redux/thunks/robotsThunk";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadRobotsActionThunk());
+    dispatch(loadRobotsThunk());
   }, [dispatch]);
 
   return (
